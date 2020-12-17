39b9613e06425eb9faa00b2e53f06539a6cc2dec
Pierre-Lloyd Parson <plparson01@gmail.com>
2020-12-17 20:10:31 +0000

    added different colours to the shapes and tidied up code

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
   let nextRandom = 0
   let timerId
   let score = 0
+  const colours = [
+    'orange',
+    'red',
+    'purple',
+    'green',
+    'blue'
+  ]
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -53,16 +60,16 @@ document.addEventListener('DOMContentLoaded', () => {
   function draw () {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
+      squares[currentPosition + index].style.backgroundColor = colours[random]
     })
   }
 
   function undraw () {
     current.forEach(index => {
       squares[currentPosition + index].classList.remove('tetromino')
+      squares[currentPosition + index].style.backgroundColor = ''
     })
   }
-
-  // timerId = setInterval(moveDown, 1000)
 
   function control (e) {
     if (e.keyCode === 37) {
@@ -87,7 +94,6 @@ document.addEventListener('DOMContentLoaded', () => {
   function freeze () {
     if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-
       random = nextRandom
       nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
@@ -102,22 +108,17 @@ document.addEventListener('DOMContentLoaded', () => {
   function moveLeft () {
     undraw()
     const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
-
     if (!isAtLeftEdge) currentPosition -= 1
-
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
       currentPosition += 1
     }
-
     draw()
   }
 
   function moveRight () {
     undraw()
     const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
-
     if (!isAtRightEdge) currentPosition += 1
-
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
       currentPosition -= 1
     }
@@ -137,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const displaySquares = document.querySelectorAll('.mini-grid div')
   const displayWidth = 4
-  let displayIndex = 0
+  const displayIndex = 0
 
   const upNextTetrominoes = [
     [1, displayWidth + 1, displayWidth * 2 + 1, 2], // lTetromino
@@ -150,9 +151,11 @@ document.addEventListener('DOMContentLoaded', () => {
   function displayShape () {
     displaySquares.forEach(square => {
       square.classList.remove('tetromino')
+      square.style.backgroundColor = ''
     })
     upNextTetrominoes[nextRandom].forEach(index => {
       displaySquares[displayIndex + index].classList.add('tetromino')
+      displaySquares[displayIndex + index].style.backgroundColor = colours[nextRandom]
     })
   }
 
@@ -171,13 +174,13 @@ document.addEventListener('DOMContentLoaded', () => {
   function addScore () {
     for (let i = 0; i < 199; i += width) {
       const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
-
       if (row.every(index => squares[index].classList.contains('taken'))) {
         score += 10
         scoreDisplay.innerHTML = score
         row.forEach(index => {
           squares[index].classList.remove('taken')
           squares[index].classList.remove('tetromino')
+          squares[index].style.backgroundColor = ''
         })
         const squaresRemoved = squares.splice(i, width)
         squares = squaresRemoved.concat(squares)
